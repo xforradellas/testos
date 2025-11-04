f3ddf90784ff6a1168a11cd3e21b59ca3a012055
Xavier Forradellas <xavier.forradellas@ajmanresa.cat>
2025-11-04 20:09:39 +0100

    netejar

--- images/web/src/pub/index.php
+++ images/web/src/pub/index.php
@@ -1,10 +1,8 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Ajt\ApiBase\{Response, Router, Request};
-use \Ajt\Test\pub\controllers\HostsController;
+use Ajt\ApiBase\{Router, Request};
 use \Ajt\Test\pub\controllers\PortalsController;
-use \Ajt\ApiBase\AuthService;
 use \Ajt\Test\pub\controllers\MenusController;
 use \Ajt\DB\Model;
 
@@ -18,15 +16,36 @@ $menusCtrl = new MenusController();
 $router = new Router();
 
 // Definició de rutes (estructura jeràrquica)
-// hosts
-$router->register('GET', '/pub/portals', [$portalsCtrl, 'getAll'],['cache' => true,'cache_ttl' => 60]);
-$router->register('GET', '/pub/portals/{id}', [$portalsCtrl, 'getById'],['cache' => true,'cache_ttl' => 60]);
+// Portals
+$router->register('GET', '/pub/portals',
+    [$portalsCtrl, 'getAll'],
+    ['cache' => true,'cache_ttl' => 60]
+);
+$router->register('GET', '/pub/portals/{id}',
+    [$portalsCtrl, 'getById'],
+    ['cache' => true,'cache_ttl' => 60]
+);
 
-$router->register('GET', '/pub/portals/{id}/menus', [$menusCtrl, 'getAll'],['cache' => true,'cache_ttl' => 60]);
-$router->register('GET', '/pub/portals/{id}/menus/{idMenu}', [$menusCtrl, 'getById'],['cache' => true,'cache_ttl' => 60]);
-$router->register('GET', '/pub/portals/{id}/menus/{idMenu}/@filariadna', [$menusCtrl, 'getFilAriadna'],['cache' => true,'cache_ttl' => 60]);
-$router->register('GET', '/pub/portals/{id}/ultimesact', [$menusCtrl, 'getUltimesAct']);
-$router->register('GET', '/pub/portals/{id}/cercar/{cerca}', [$menusCtrl, 'getCerca'],['cache' => true,'cache_ttl' => 60]);
+// Menus
+$router->register('GET', '/pub/portals/{id}/menus',
+    [$menusCtrl, 'getAll'],
+    ['cache' => true,'cache_ttl' => 60]
+);
+$router->register('GET', '/pub/portals/{id}/menus/{idMenu}',
+    [$menusCtrl, 'getById'],
+    ['cache' => true,'cache_ttl' => 60]
+);
+$router->register('GET', '/pub/portals/{id}/menus/{idMenu}/@filariadna',
+    [$menusCtrl, 'getFilAriadna'],
+    ['cache' => true,'cache_ttl' => 60]
+);
+$router->register('GET', '/pub/portals/{id}/ultimesact',
+    [$menusCtrl, 'getUltimesAct']
+);
+$router->register('GET', '/pub/portals/{id}/cercar/{cerca}',
+    [$menusCtrl, 'getCerca'],
+    ['cache' => true,'cache_ttl' => 60]
+);
 
 // Llança el router
 $router->dispatch($request);
